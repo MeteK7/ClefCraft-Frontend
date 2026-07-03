7ffabd308422ee2d042663808ed8ca1ebfa5852d
Mete <metekaba96@gmail.com>
2026-07-03 22:45:24 +0300

    update: app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,17 +9,64 @@ import { PlayalongComponent } from './pages/playalong/playalong.component';
 import { MetronomeComponent } from './pages/metronome/metronome.component';
 import { TunerComponent } from './pages/tuner/tuner.component';
 import { ManagementComponent } from './pages/management/management.component';
+import { authGuard } from './guards/auth.guard';
+import { ProtectedWorkspaceComponent } from './pages/protected-workspace/protected-workspace.component';
 
 export const routes: Routes = [
+
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+
   { path: 'home', component: HomeComponent },
+
   { path: 'login', component: LoginComponent },
+
   { path: 'registration', component: RegistrationComponent },
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'event-tracker', component: EventTrackerComponent },
-  { path: 'board', component: BoardComponent },
-  { path: 'playalong', component: PlayalongComponent },
-  { path: 'metronome', component: MetronomeComponent },
-  { path: 'tuner', component: TunerComponent },
-  { path: 'management', component: ManagementComponent }
+
+  {
+    path: 'calendar',
+    component: CalendarComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'event-tracker',
+    component: EventTrackerComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'board',
+    component: BoardComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'management',
+    component: ManagementComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'playalong',
+    component: PlayalongComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'metronome',
+    component: MetronomeComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'tuner',
+    component: TunerComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'protected',
+    component: ProtectedWorkspaceComponent
+  }
+
 ];
