ccc63d9edd868e5ee49348dac795cd207614ee19
Mete <metekaba96@gmail.com>
2026-08-22 21:18:06 +0300

    modify: item detail dialog for activity log

--- src/app/pages/item-detail-dialog/item-detail-dialog.component.spec.ts
+++ src/app/pages/item-detail-dialog/item-detail-dialog.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
-import { ItemDetailDialogComponent } from './item-detail-dialog.component';
+import { ItemDetailDialogComponent, ItemDetailDialogData } from './item-detail-dialog.component';
 
 describe('ItemDetailDialogComponent', () => {
   let component: ItemDetailDialogComponent;
   let fixture: ComponentFixture<ItemDetailDialogComponent>;
 
+  const dialogData: ItemDetailDialogData = {
+    item: null,
+    boardId: 1
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ItemDetailDialogComponent]
+      imports: [ItemDetailDialogComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+        provideNoopAnimations(),
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+        { provide: MatDialogRef, useValue: { close: () => { } } }
+      ]
     })
     .compileComponents();
 
