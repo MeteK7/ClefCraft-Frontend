cbf79663a15bfc27f58b9288bd377d1d58b2e51f
Mete <metekaba96@gmail.com>
2026-08-24 16:37:54 +0300

    add: history feature

--- src/app/pages/calendar-dialog/calendar-dialog.component.spec.ts
+++ src/app/pages/calendar-dialog/calendar-dialog.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 import { CalendarDialogComponent } from './calendar-dialog.component';
 
@@ -8,7 +13,14 @@ describe('CalendarDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CalendarDialogComponent]
+      imports: [CalendarDialogComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+        provideNoopAnimations(),
+        { provide: MAT_DIALOG_DATA, useValue: { eventData: null, date: new Date() } }
+      ]
     })
     .compileComponents();
 
